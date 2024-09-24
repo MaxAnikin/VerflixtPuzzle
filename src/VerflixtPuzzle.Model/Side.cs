e48acf78cfeadd8d1507cde558890dde85f4f76d
MakAni <maksym.anikin@omicronenergy.com>
2024-09-24 15:43:26 +0200

    initial commit

--- src/VerflixtPuzzle.Model/Side.cs
+++ src/VerflixtPuzzle.Model/Side.cs
@@ -1,30 +1,35 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
 
 namespace VerflixtPuzzle.Model
 {
-    public enum SideTypeOperator
-    {
-        Xor,
-        And
-    }
+    //public enum SideTypeOperator
+    //{
+    //    Xor,
+    //    And
+    //}
 
-    public abstract record SideType;
+    //public abstract record SideType;
 
-    public abstract record XorSideType();
+    //public abstract record XorSideType();
 
-    public record Side(XorSideType Xor, int And)
-    {
-        public bool Fit(Side s2)
-        {
-            return 
-        }
-    }
+    //public record Side(XorSideType Xor, int And)
+    //{
+    //    public bool Fit(Side s2)
+    //    {
+    //        return 
+    //    }
+    //}
 
-    public static class SideExtensions
-    {
-        public static bool Fit(this Side s1, Side s2)
-        {
-            s1.
-        }
-    }
+    //public static class SideExtensions
+    //{
+    //    public static bool Fit(this Side s1, Side s2)
+    //    {
+    //        s1.
+    //    }
+    //}
+
+    public abstract record Side;
+
+    public record TSide(BodyPart part, Color ) : Side;
 }
