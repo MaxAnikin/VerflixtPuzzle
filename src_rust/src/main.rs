9fa2989ce2c8f8715422d93851ff21e3ff18d081
MakAni <maksym.anikin@omicronenergy.com>
2025-10-09 09:18:02 +0200

    vsc run and build conf

--- src_rust/src/main.rs
+++ src_rust/src/main.rs
@@ -12,22 +12,24 @@ trait ConsoleDisplayablePuzzle {
 impl ConsoleDisplayablePuzzle for SquarePuzzle
 {
     fn show_in_console(&self){
-        println!("{} {}", "I am a Square Puzzle. Current orientation: ", self.orientation.to_string());
+        println!("{} {}", "I am a Square Puzzle. Current orientation: ", self.orientation);
     }
 }
 
-// test the example with `cargo run --example most_simple`
-fn main() -> io::Result<()> {
+fn main() {
     println!("{}", "Starting a puzzle game... Enjoy!".green());
 
     println!("{}", "Creating a puzzle...");
 
     let puzzle: SquarePuzzle = create_puzzle();
     puzzle.show_in_console();
-    
-    let mut buffer: String = String::new();
-    io::stdin().read_line(&mut buffer)?;
-    Ok(())
+
+    println!("{}", "Game is over. Press ENTER to close.".green());
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Read input error."
+    );
 }
 
 fn create_puzzle() -> SquarePuzzle {
